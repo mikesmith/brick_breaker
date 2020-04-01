18d40d01f56ecfa73c039c968f109219e750e81c
Mike Smith <michael.eugene.smith@gmail.com>
2020-03-31 21:24:12 -0700

    Clean up player movement

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import arcade
 
-from constants import SCREEN_WIDTH, WALL_WIDTH
+from constants import SCREEN_WIDTH, WALL_WIDTH, MOVEMENT_SPEED
 
 
 class Player(arcade.Sprite):
@@ -12,21 +12,23 @@ class Player(arcade.Sprite):
         self.center_x = SCREEN_WIDTH / 2
         self.center_y = 50
 
+        # Track the current state of what key is pressed
+        self.left_pressed = False
+        self.right_pressed = False
+
     def on_key_press(self, symbol: int, modifiers: int):
         """Handle user keyboard input.
 
-        J/K: Move Left or Right
         Arrows: Move Left or Right
 
         Arguments:
             symbol {int} -- Which key was pressed
             modifiers {int} -- Which modifiers were pressed
         """
-        if symbol == arcade.key.J or symbol == arcade.key.LEFT:
-            self.change_x = -250
-
-        if symbol == arcade.key.L or symbol == arcade.key.RIGHT:
-            self.change_x = 250
+        if symbol == arcade.key.LEFT:
+            self.left_pressed = True
+        elif symbol == arcade.key.RIGHT:
+            self.right_pressed = True
 
     def on_key_release(self, symbol: int, modifiers: int):
         """Undo movement vectors when movement keys are released.
@@ -35,15 +37,20 @@ class Player(arcade.Sprite):
             symbol {int} -- Which key was pressed
             modifiers {int} -- Which modifiers were pressed
         """
-        if (
-            symbol == arcade.key.J
-            or symbol == arcade.key.L
-            or symbol == arcade.key.LEFT
-            or symbol == arcade.key.RIGHT
-        ):
-            self.change_x = 0
+        if symbol == arcade.key.LEFT:
+            self.left_pressed = False
+        elif symbol == arcade.key.RIGHT:
+            self.right_pressed = False
 
     def on_update(self, delta_time: float):
+        self.change_x = 0
+        self.change_y = 0
+
+        if self.left_pressed and not self.right_pressed:
+            self.change_x = -MOVEMENT_SPEED
+        elif self.right_pressed and not self.left_pressed:
+            self.change_x = MOVEMENT_SPEED
+
         self.center_x = self.center_x + self.change_x * delta_time
         self.center_y = self.center_y + self.change_y * delta_time
 
